471a9219924bfeebf26770e2c86297f3474bc97c
Ben McIntyre <ben@bens-mbp.attlocal.net>
2022-11-15 19:22:10 -0800

    Part 5 FInished

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,24 +1,28 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
+const place = require("../models/places.js");
 
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/thai-food-image.jpeg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/latte-photo.jpeg",
-    },
-  ];
   res.render("places/index", { places });
+});
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = "/images/town.jpeg";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 module.exports = router;
