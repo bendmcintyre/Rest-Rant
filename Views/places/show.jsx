6b08e7962fb6809bfc60cc5887da2aff945d4370
Ben McIntyre <ben@bens-mbp.attlocal.net>
2022-12-03 11:19:43 -0800

    Part 9 Finished

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -5,27 +5,35 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <img src={data.place.pic} alt={data.place.name} />
+                <h3>Located in {data.place.city}, {data.place.state}</h3>
+                </div>
+                <div className="col-sm-6">
+                <h1>{data.place.name}</h1>
                 <h2>Rating</h2>
-                <p>Not Rated</p>
-            </div>
-            <div>
+                    {rating}
+                <br />
                 <h2>Description</h2>
-                <p>Located in Phoenix, AZ and serving Coffee, Bakery</p>
-            </div>
-            <div>
-                <h2>Comments</h2>
-                <p>No comments yet!</p>
-            </div>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>     
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <h3>{data.place.showEstablished()}</h3>
+                <h4>Serving {data.place.cuisines}</h4>
+                <a
+                    href={`/places/${data.place.id}/edit`}
+                    className="btn btn-warning"
+                >
+                    Edit
+                </a>
+                <form
+                    method="POST"
+                    action={`/places/${data.place.id}?_method=DELETE`}
+                >
                 <button type="submit" className="btn btn-danger">
                     Delete
                 </button>
-            </form>
+                </form>
+                </div>
+            </div>
           </main>
         </Def>
     )
